da8a8f90ca83828fbee5aa1a4febadb3957486bb
Richard <richrd.feciskanin@uniba.sk>
2021-02-26 12:16:48 +0100

    base path use

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,7 @@ function MyApp({ Component, pageProps }) {
           name="Description"
           content={text[locale].desc}
         />
-        <base href={config.basePath || '' + '/'} />
+        <base href={(config.basePath || '') + '/'} />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
