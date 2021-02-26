fa2da6a0b38372658d9c54c26c9ded7437b65613
Richard <richrd.feciskanin@uniba.sk>
2021-02-26 12:51:29 +0100

    fix relative

--- pages/404.js
+++ pages/404.js
@@ -15,7 +15,7 @@ export default function FourOhFour( props ) {
   return (
     <div className="flex flex-col items-center justify-self-center py-6 px-2 text-k-brown text-opacity-50">
       <div className="relative">
-        <img className="h-52 md:h-72" src={`/images/desert.svg`} />
+        <img className="h-52 md:h-72" src={`images/desert.svg`} />
         <div className="text-7xl md:text-8xl text-white text-opacity-20 text-center absolute inset-x-0 bottom-4 md:bottom-6">404</div>
       </div>
       <div className="text-lg md:text-xl text-center mb-8 md:mb-10 -mt-3 md:-mt-4">{text[locale]}</div>
