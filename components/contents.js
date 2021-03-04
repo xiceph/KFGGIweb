2d8b3a058b5ecdb002716dbfb878a2be8bcb2dfe
Richard <richrd.feciskanin@uniba.sk>
2021-03-04 11:58:47 +0100

    update

--- components/contents.js
+++ components/contents.js
@@ -23,7 +23,9 @@ export default function Contents( props ) {
     if( levels > 1 && item.type == "h4" ) {
       const id = `sect_${level1}_${++level2}`;
       contents.push(
-        <div className="pl-4 md:pl-6 pb-1" key={ id } ><Link href={`${router.asPath.split('#')[0]}#${id}`}>{ item.props.children }</Link></div>
+        <div className="pl-3 md:pl-4 pb-1" key={ id } >
+          <Link href={`${router.asPath.split('#')[0]}#${id}`}>{ item.props.children }</Link>
+        </div>
       );
       
       return ( <h4 className="anchor" { ...item.props } ><span id={ id } ></span>{ item.props.children }</h4> )
@@ -35,7 +37,7 @@ export default function Contents( props ) {
   
   return (
     <>
-      <div className="border-l-3 md:border-l-4 border-gray-300 pl-4 md:pl-6 py-2 md:py-3">
+      <div className="border-l-3 md:border-l-4 border-gray-300 pl-3 md:pl-5 py-2 md:py-3">
         {contents}
       </div>
       {rest}
