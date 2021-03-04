5a5b814c63d9e3df812ec019334dbaaa92328d1f
Richard <richrd.feciskanin@uniba.sk>
2021-03-04 14:43:37 +0100

    update

--- components/personalInfo.js
+++ components/personalInfo.js
@@ -8,14 +8,14 @@ export default function PersonalInfo( props ) {
   return (
     <>
       <div className="flex flex-col md:flex-row items-center">
-        <div className="w-44 h-44 bg-gray-200 bg-opacity-60 md:mr-6 rounded-full">
+        <div className="w-44 h-44 bg-gray-200 bg-opacity-60 mb-6 md:mb-1 md:mr-8 rounded-full">
           { !photo && (
             <svg className={`h-44 w-44 p-4 inline text-gray-300 text-opacity-80 align-text-middle`} viewBox="0 0 24 24">
               <path fill="currentColor" d="M9,11.75A1.25,1.25 0 0,0 7.75,13A1.25,1.25 0 0,0 9,14.25A1.25,1.25 0 0,0 10.25,13A1.25,1.25 0 0,0 9,11.75M15,11.75A1.25,1.25 0 0,0 13.75,13A1.25,1.25 0 0,0 15,14.25A1.25,1.25 0 0,0 16.25,13A1.25,1.25 0 0,0 15,11.75M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20C7.59,20 4,16.41 4,12C4,11.71 4,11.42 4.05,11.14C6.41,10.09 8.28,8.16 9.26,5.77C11.07,8.33 14.05,10 17.42,10C18.2,10 18.95,9.91 19.67,9.74C19.88,10.45 20,11.21 20,12C20,16.41 16.41,20 12,20Z" />
           </svg>) 
           }
         </div>
-        <div>
+        <div className="flex-grow self-stretch md:self-center">
           <div className="my-2">
             <Door className="text-gray-400" /> {room || ''}
           </div>
@@ -26,7 +26,7 @@ export default function PersonalInfo( props ) {
             <At className="text-gray-400" /> <Email parts={email} />
           </div>
           { web && (
-            <div className="mt-3">
+            <div className="mt-2">
               <Pointer className="text-gray-400" /> <a href={web} target="_blank">{web}<OpenExt /></a>
             </div>
           )}            
