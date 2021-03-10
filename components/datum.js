44da21cd7d07ad4917d1aa939aa6b24560e447ce
Richard <richrd.feciskanin@uniba.sk>
2021-03-10 09:28:30 +0100

    update

--- components/datum.js
+++ components/datum.js
@@ -1,10 +1,10 @@
 export default function Datum( props ) {
-  const { date } = props;
+  const { date, className } = props;
   
   const options = { dateStyle: 'short' };
   
   return (
-    <span className="rounded-info text-sm text-gray-600 pl-2 pr-3 py-1 mr-3" >
+    <span className={`rounded-info text-sm text-gray-600 pl-2 pr-3 py-1 mr-3 ${className}`} >
       <svg className="h-6 w-6 mr-1 inline-block relative -top-0.5 text-gray-500" viewBox="0 0 24 24">
         <path fill="currentColor" d="M9,10V12H7V10H9M13,10V12H11V10H13M17,10V12H15V10H17M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5A2,2 0 0,1 5,3H6V1H8V3H16V1H18V3H19M19,19V8H5V19H19M9,14V16H7V14H9M13,14V16H11V14H13M17,14V16H15V14H17Z" />
       </svg>
---
+++ pages/vyskum/projekty.js
@@ -0,0 +1,294 @@
+import { OpenExt } from "@components/icons";
+
+export default function Projekty() {
+  return (
+    <article>
+      <h1>Prehľad riešených projektov</h1>
+
+      <table><thead>
+        <tr>
+            <th>Trvanie</th>
+            <th>Schéma</th>
+            <th>Číslo</th>
+            <th>Zodpovedný riešiteľ / vedúci</th>
+            <th>Názov</th>
+        </tr>
+        </thead><tbody>
+        <tr>
+            <td>01.01.2020 – 31.12.2022</td>
+            <td>VEGA</td>
+            <td>1/0555/20</td>
+            <td>RNDr. Igor <b>Matečný</b>, PhD.</td>
+            <td><a href="http://fns.uniba.sk/ismonitoringbioty" target="_blank" data-htmlarea-external="1">Informačný systém monitoring bioty v rámci hodnotenia vplyvu VD Gabčíkovo <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2019 – 31.12.2022</td>
+            <td>VEGA</td>
+            <td>1/0247/19</td>
+            <td>doc. RNDr. Vladimír <b>Falťan</b>, PhD.</td>
+            <td><a href="http://fns.uniba.sk/hodnotenie" target="_blank" data-htmlarea-external="1">Hodnotenie dynamiky využitia krajiny a zmien krajinnej pokrývky <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2017 – 31.12.2019</td>
+            <td>VEGA</td>
+            <td>1/0781/17</td>
+            <td>doc. Ing. Peter <b>Pišút</b>, PhD.</td>
+            <td><a href="http://fns.uniba.sk/riecnealuvia" target="_blank" data-htmlarea-external="1">Riečne alúviá ako archívy prírody a ľudskej civilizácie <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2017 – 31.12.2019</td>
+            <td>KEGA</td>
+            <td>003UK-4/2017</td>
+            <td>RNDr. Ivan <b>Ružek</b>, PhD.</td>
+            <td><a href="http://fns.uniba.sk/terennevyucovanie" target="_blank" data-htmlarea-external="1">Terénne vyučovanie geovied s využitím vybraných náučných chodníkov <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.07.2016 – 31.10.2020</td>
+            <td>APVV</td>
+            <td>APVV-15-0054</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td><a href="http://fns.uniba.sk/segment" target="_blank" data-htmlarea-external="1">Fyzikálne založená segmentácia georeliéfu a jej geovedné aplikácie <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.07.2016 – 31.12.2019</td>
+            <td>APVV</td>
+            <td>APVV-15-0597</td>
+            <td>RNDr. Igor <b>Matečný</b>, PhD.</td>
+            <td><a href="http://fns.uniba.sk/precfarm" target="_blank" data-htmlarea-external="1">Využitie geoekologických podkladov v rámci zavádzania precízneho poľnohospodárstva <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2016 – 31.12.2019</td>
+            <td>VEGA</td>
+            <td>1/0602/16</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td><a href="http://fns.uniba.sk/datovanie" target="_blank" data-htmlarea-external="1">Datovanie hlavných etáp vývoja reliéfu Západných Karpát <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2016 – 31.12.2018</td>
+            <td>VEGA</td>
+            <td>1/0421/16</td>
+            <td>doc. RNDr. Vladimír <b>Falťan</b>, PhD.</td>
+            <td><a href="http://fns.uniba.sk/AnalyzaZmien" target="_blank" data-htmlarea-external="1">Analýza zmien krajinnej pokrývky v kontexte environmentálnych hybných síl <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2016 – 31.12.2017</td>
+            <td>KEGA</td>
+            <td>080UK-4/2016</td>
+            <td>doc. RNDr. Vladimír <b>Falťan</b>, PhD.</td>
+            <td><a href="http://fns.uniba.sk/ucebnicametodyvyskumulc/" target="_blank" data-htmlarea-external="1">Tvorba vysokoškolskej učebnice „Metódy výskumu krajinnej pokrývky“ <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>2015 – 2016</td>
+            <td>Štipendijný program EHP Slovensko</td>
+            <td>EEA/EHP-SK06-IV-V-02</td>
+            <td>Mgr. Juraj <b>Holec</b>, PhD. (riešiteľ z katedry)</td>
+            <td><a href="https://eeagrants.org/project-portal/project/SK06-0019" target="_blank" data-htmlarea-external="1">PARtnership Against GEOhazards <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.07.2012 – 31.12.2015</td>
+            <td>KEGA</td>
+            <td>088UK-4/2013</td>
+            <td>RNDr. Ivan <b>Ružek</b>, PhD.</td>
+            <td>Nové trendy v geovedách – geovedné vzdelávanie učiteľov</td>
+        </tr>
+        <tr>
+            <td>01.07.2012 – 31.12.2015</td>
+            <td>APVV</td>
+            <td>APVV-0625-11</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td><a href="http://fns.uniba.sk/neosynteza" target="_blank" data-htmlarea-external="1">Nová syntéza vývoja reliéfu Západných Karpát – príprava databázy pre testovanie kľúčových hypotéz <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2013 – 31.12.2015</td>
+            <td>IVF Strategic Grant</td>
+            <td>31210058</td>
+            <td>Mgr. Monika <b>Šulc Michalková</b>, PhD. et PhD.</td>
+            <td>Detailed aerial mapping and flood impact monitoring in the V4 region</td>
+        </tr>
+        <tr>
+            <td>01.06.2012 – 30.11.2012</td>
+            <td>IVF Small Grants </td>
+            <td>11210039</td>
+            <td>Mgr. Monika <b>Šulc Michalková</b>, PhD. et PhD.</td>
+            <td>Evaluation of a new remote sensing methodology for detailed international mapping in the V4 region</td>
+        </tr>
+        <tr>
+            <td>01.01.2011 – 31.12.2013</td>
+            <td>VEGA</td>
+            <td>1/0937/11</td>
+            <td>RNDr. Marián <b>Jenčo</b>, PhD.</td>
+            <td>Morfometrická analýza georeliéfu a jej význam pri štúdiu procesov a zmien v krajine</td>
+        </tr>
+        <tr>
+            <td>01.01.2011 – 31.12.2013</td>
+            <td>VEGA</td>
+            <td>2/0037/11</td>
+            <td>Ing. Peter <b>Pišút</b>, PhD.</td>
+            <td>Ostrovy vegetácie ako zdroje biodiverzity vo veľkomeste</td>
+        </tr>
+        <tr>
+            <td>01.09.2009 – 31.08.2012 (31.12.2012)</td>
+            <td>APVV</td>
+            <td>LPP-0130-09</td>
+            <td>RNDr. Mária <b>Bizubová</b> / RNDr. Ivan <b>Ružek</b>, PhD.</td>
+            <td>Geovedy pre každého</td>
+        </tr>
+        <tr>
+            <td>01.01.2009 – 31.12.2011</td>
+            <td>VEGA</td>
+            <td>1/0362/09</td>
+            <td>Ing. Peter <b>Pišút</b>, PhD.</td>
+            <td>Využitie archívnych prameňov pri rekonštrukcii vývoja korýt a prírodného prostredia riečnych nív</td>
+        </tr>
+        <tr>
+            <td>01.01.2009 – 31.12.2011</td>
+            <td>VEGA</td>
+            <td>1/0434/09</td>
+            <td>doc. RNDr. Miloš <b>Stankoviansky</b>, CSc.</td>
+            <td><a href="http://fns.uniba.sk/index.php?id=20418" target="_blank" data-htmlarea-external="1">Recentný vývoj reliéfu a geomorfologický efekt extrémnych udalostí <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>03.06.2008 – 31.12.2011 (30.06.2012)</td>
+            <td>APVV</td>
+            <td>ESF-EC-0006-07</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td><a href="http://fns.uniba.sk/SourceSink" target="_blank" data-htmlarea-external="1">Od výzdvihu k poklesu: integrované hodnotenie prírodných hrozieb prostredníctvom kvantifikácie transportu materiálu z pohorí do aktívnych sedimentárnych paniev <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>26.05.2008 – 30.04.2009</td>
+            <td>APVV</td>
+            <td>APVV-51-040805</td>
+            <td>Ing. Peter <b>Pišút</b>, PhD.</td>
+            <td>Vplyv zmien kvality ovzdušia mestskej aglomerácie Bratislava na lišajníky</td>
+        </tr>
+        <tr>
+            <td>01.01.2007 – 31.12.2009</td>
+            <td>VEGA</td>
+            <td>1/4042/07</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td>Geografické polia a výskum prírodných hrozieb (hazardov) a rizík</td>
+        </tr>
+        <tr>
+            <td>2006 – 2013</td>
+            <td>IAG/AIG </td>
+            <td>CarBalDinRWG</td>
+            <td>doc. RNDr. Miloš <b>Stankoviansky</b>, CSc.</td>
+            <td><a href="http://fns.uniba.sk/carbaldin" target="_blank" data-htmlarea-external="1">Recent Landform Evolution in the Carpatho-Balkan-Dinaric Area <OpenExt /></a></td>
+        </tr>
+        <tr>
+            <td>01.01.2006 – 31.12.2008</td>
+            <td>KEGA</td>
+            <td>3/4270/06</td>
+            <td>RNDr. Mária <b>Bizubová</b></td>
+            <td>Ako učiť o dejinách – slovenské stredné školstvo v európskej dimenzii dejepisného vzdelávania</td>
+        </tr>
+        <tr>
+            <td>01.01.2006 – 31.12.2008</td>
+            <td>VEGA</td>
+            <td>1/3052/06</td>
+            <td>doc. RNDr. Milan <b>Trizna</b>, PhD. / RNDr. Vladimír <b>Falťan</b>, PhD.</td>
+            <td>Analýza zmien vybraných parametrov krajinného systému na území TANAP-u postihnutom veternou kalamitou</td>
+        </tr>
+        <tr>
+            <td>01.01.2006 – 31.12.2008</td>
+            <td>VEGA</td>
+            <td>1/3051/06</td>
+            <td>doc. RNDr. Miloš <b>Stankoviansky</b>, CSc.</td>
+            <td>Hodnotenie geomorfologického efektu a environmentálneho dopadu ronových a orbových procesov</td>
+        </tr>
+        <tr>
+            <td>13.11.2006 – 30.11.2009 (31.03.2008)</td>
+            <td>APVV</td>
+            <td>LPP-0232-06</td>
+            <td>doc. RNDr. Miloš <b>Stankoviansky</b>, CSc.</td>
+            <td>Aplikácia digitálneho modelu reliéfu s vysokým rozlíšením na modelovanie prírodných hrozieb a potenciálov (prípadová štúdia Bratislava)</td>
+        </tr>
+        <tr>
+            <td>2006 – 2007</td>
+            <td>MVTP</td>
+            <td>SK-CZ-05106</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td>Geomorfologický a environmentálny informačný systém ako nástroj výskumu a manažmentu krajiny</td>
+        </tr>
+        <tr>
+            <td>2005 – 2007</td>
+            <td>KEGA</td>
+            <td>3/3023/05</td>
+            <td>RNDr. Mária <b>Bizubová</b></td>
+            <td>Katalóg náučných chodníkov – učebná pomôcka pre základné a stredné školy</td>
+        </tr>
+        <tr>
+            <td>2004 – 2006</td>
+            <td>VEGA</td>
+            <td>1/1037/04</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td>Rozvoj geoinformačne orientovaných metód základného geomorfologického a geoekologického výskumu a mapovania pre potreby environmentálnych evaluácií</td>
+        </tr>
+        <tr>
+            <td>2004 – 2005</td>
+            <td>MVTP</td>
+            <td>CZE 116</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td>Geomorfologický informačný systém ako báza environmentálnych aplikácií</td>
+        </tr>
+        <tr>
+            <td>2004 – 2005</td>
+            <td>MVTP</td>
+            <td>CZE 071</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td>Geomorfologický výskum a prírodné hazardy v južnej časti česko-slovenského pomedzia</td>
+        </tr>
+        <tr>
+            <td>2004 – 2007</td>
+            <td>COST</td>
+            <td>COST Action 634</td>
+            <td>doc. RNDr. Miloš <b>Stankoviansky</b>, CSc.</td>
+            <td>On- and off-site environmental impacts of runoff and erosion</td>
+        </tr>
+        <tr>
+            <td>2003 – 2005</td>
+            <td>VEGA</td>
+            <td>1/0038/03</td>
+            <td>doc. RNDr. Miloš <b>Stankoviansky</b>, CSc.</td>
+            <td>Tranformácia reliéfu a pôdy následkom environmentálnych zmien</td>
+        </tr>
+        <tr>
+            <td>2003 – 2005</td>
+            <td>VEGA</td>
+            <td>1/0042/03</td>
+            <td>doc. RNDr. Milan <b>Trizna</b>, PhD.</td>
+            <td>Hodnotenie vplyvu vybraných krajinných prvkov na zrážkovo-odtokové procesy</td>
+        </tr>
+        <tr>
+            <td>2001 – 2003</td>
+            <td>VEGA</td>
+            <td>1/8246/01</td>
+            <td>prof. RNDr. Jozef <b>Minár</b>, CSc.</td>
+            <td>Komplexný monitorovací výskum a informačný systém prírodných katastrôf v Slovenskej republike (príprava metodiky pre potreby ekologického riadenia a optimalizácie krajiny)</td>
+        </tr>
+        <tr>
+            <td>2001 – 2003</td>
+            <td>VEGA</td>
+            <td>1/8203/01</td>
+            <td>RNDr. Peter <b>Tremboš</b>, PhD.</td>
+            <td>Analýza využitia geografických informácií pri spracúvaní environmentálne zameraných dokumentov</td>
+        </tr>
+        <tr>
+            <td>2000 – 2003</td>
+            <td>COST</td>
+            <td>COST action 623</td>
+            <td>doc. RNDr. Miloš <b>Stankoviansky</b>, CSc.</td>
+            <td>Soil erosion and global change</td>
+        </tr>
+        <tr>
+            <td>2000 – 2002</td>
+            <td>VEGA</td>
+            <td>1/7333/20</td>
+            <td>doc. RNDr. Milan <b>Trizna</b>, PhD.</td>
+            <td>Hodnotenie povodňového ohrozenia územia ako aktuálnej prírodnej hrozby a súvisiacich prírodných rizík</td>
+        </tr>
+      </tbody></table>
+     </article>
+  )
+}
+
