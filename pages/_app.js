94b636eb57a7631354e6bc50e59f2f773119b8a5
Richard <richrd.feciskanin@uniba.sk>
2021-03-13 09:36:49 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -55,7 +55,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#8ce4ff" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       
