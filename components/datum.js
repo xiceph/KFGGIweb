b916e91c488ecee91da96430b823d41ee40d2363
Richard <richrd.feciskanin@uniba.sk>
2021-03-31 12:10:00 +0200

    update

--- components/datum.js
+++ components/datum.js
@@ -2,10 +2,16 @@ export default function Datum( props ) {
   const { date, className } = props;
   
   const options = { dateStyle: 'short' };
+  const today = new Date();
+  const isToday = date.getDate() == today.getDate() 
+    && date.getMonth() == today.getMonth() 
+    && date.getFullYear() == today.getFullYear();
+    
+  const type = isToday ? 'rounded-info-strong' : 'rounded-info'
   
   return (
-    <span className={`rounded-info text-sm text-gray-600 pl-2 pr-3 py-1 mr-3 ${className}`} >
-      <svg className="h-6 w-6 mr-1 inline-block relative -top-0.5 text-gray-500" viewBox="0 0 24 24">
+    <span className={`${type} text-sm pl-2 pr-3 py-1 mr-3 ${className}`} >
+      <svg className="h-6 w-6 mr-1 inline-block relative -top-0.5" viewBox="0 0 24 24">
         <path fill="currentColor" d="M9,10V12H7V10H9M13,10V12H11V10H13M17,10V12H15V10H17M19,3A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5A2,2 0 0,1 5,3H6V1H8V3H16V1H18V3H19M19,19V8H5V19H19M9,14V16H7V14H9M13,14V16H11V14H13M17,14V16H15V14H17Z" />
       </svg>
       {date.toLocaleDateString()}
