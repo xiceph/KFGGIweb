25098bb3fd8ea19f03da4b02aae62ee1512b1c55
Richard <richrd.feciskanin@uniba.sk>
2021-04-02 08:52:11 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -80,11 +80,6 @@ export default function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              <div className="mb-1.5"><Datum date={new Date("2021-03-31")}></Datum></div>
-              <h5 className="font-bold mb-1">Termín podávania prihlášok</h5>
-              <div>Na bakalárske študijné programy uskutočňované na Prírodovedeckej fakulte UK v akad. roku 2021/2022. Detaily o <a href="https://fns.uniba.sk/studium/uchadzaci-o-studium/prijimacie-konanie/bakalarsky-stupen-studia/" taget="_blank">prijímacom konaní na fakulte <OpenExt /></a>.</div>
-            </div>
-            <div className="py-4 first:pt-0">
               <div className="mb-1.5"><Datum date={new Date("2021-04-21")}></Datum></div>
               <h5 className="font-bold mb-2">Prednáška: Nebezpečné lavíny</h5>
               <div>
