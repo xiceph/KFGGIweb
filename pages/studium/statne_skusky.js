2a9e5df46846f9e215bd10df7e915235601e5358
Richard <richrd.feciskanin@uniba.sk>
2021-05-12 10:03:48 +0200

    update

--- pages/studium/statne_skusky.js
+++ pages/studium/statne_skusky.js
@@ -9,9 +9,9 @@ export default function Statnice() {
       <Contents levels={1} >
         <h3 className="border-l-3 md:border-l-4 border-k-brown pl-3 md:pl-5 py-2 md:py-3">Bakalárske študijné programy</h3>
         
-        <h4>Termíny</h4>
-        <p>Obhajoba bakalárskej práce ZEP  21. 6. 2021.</p>
-        <p>Obhajoba bakalárskej práce ZKI  22. 6. 2021.</p>
+        <h4>Termíny 2020/21</h4>
+        <p>Obhajoba bakalárskej práce ZEP:  21. 6. 2021.</p>
+        <p>Obhajoba bakalárskej práce ZKI:  22. 6. 2021.</p>
         
         <h4>Povinné predmety</h4>
         <ol>
@@ -26,9 +26,9 @@ export default function Statnice() {
         
         <h3 className="border-l-3 md:border-l-4 border-k-green pl-3 md:pl-5 py-2 md:py-3">Magisterský študijný program</h3>
         
-        <h4>Termíny</h4>
-        <p>mZ-FI/FYZ  7. až 9. 6. 2021.</p>
-        <p>mZ-FI/GIS  3. 6. 2021 obhajoba diplomovej práce, 10. 6. 2021 ústna skúška.</p>
+        <h4>Termíny 2020/21</h4>
+        <p>mZ-FI/FYZ:  7. - 9. 6. 2021.</p>
+        <p>mZ-FI/GIS:  3. 6. 2021 obhajoba diplomovej práce, 10. 6. 2021 ústna skúška.</p>
         
         <h4>Povinné predmety</h4>
         <ol>
