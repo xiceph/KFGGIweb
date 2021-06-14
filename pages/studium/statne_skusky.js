a6d5ee8f6fa4f108aa6f0bb9b6e37638fd3b9e6d
Richard <richard.feciskanin@uniba.sk>
2021-06-14 16:39:20 +0200

    update

--- pages/studium/statne_skusky.js
+++ pages/studium/statne_skusky.js
@@ -11,7 +11,7 @@ export default function Statnice() {
         
         <h4>Termíny 2020/21</h4>
         <p>Obhajoba bakalárskej práce ZEP:  21. 6. 2021.</p>
-        <p>Obhajoba bakalárskej práce ZKI:  22. 6. 2021.</p>
+        <p>Obhajoba bakalárskej práce ZKI:  22. 6. 2021 (<a href="docs/ObhajobyBc2021.pdf">harmonogram [pdf]</a>).</p>
         
         <h4>Povinné predmety</h4>
         <ol>
