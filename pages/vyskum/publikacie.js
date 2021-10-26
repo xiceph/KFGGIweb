3caa58b3fd07a0e8112a68a6e89c91b74b4c9dbb
Richard Runner <raner@gis.fns.uniba.sk>
2021-10-26 14:25:09 +0200

    update

--- pages/vyskum/publikacie.js
+++ pages/vyskum/publikacie.js
@@ -6,7 +6,7 @@ export default function Publikacie() {
     <article>
       <h1>Publikačná činnosť</h1>
       <p>
-        Evidencia publikácií katedry v <a href="https://alis.uniba.sk:8444/search/query?match_1=SHOULD&field_1=ud14&term_1=ukoprzkg+ukoprzfg&sort=dateNewest&theme=EPC" target="_blank">databáze Akademickej knižnice Univerzity Komenského v Bratislave <OpenExt /></a>
+        Evidencia publikácií katedry v <a href="https://alis.uniba.sk:8444/search/query?match_1=SHOULD&field_1=ud14&term_1=ukoprzfggi&sort=dateNewest&theme=EPC" target="_blank">databáze Akademickej knižnice Univerzity Komenského v Bratislave <OpenExt /></a>
         <br/>(pozn. Evidencia starších publikácií je neúplná.)
       </p>
     </article>
