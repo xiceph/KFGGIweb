c40b901dc5c47f28c1a61533353f979c3cc346af
Richard <richard.feciskanin@uniba.sk>
2022-01-27 11:01:14 +0100

    update geokarto

--- pages/geokarto2022/index.js
+++ pages/geokarto2022/index.js
@@ -12,9 +12,9 @@ export default function GeoKarto( props ) {
         <title>Konferencia GeoKARTO 2022</title>
       </Head>
       <h1>Konferencia GeoKARTO 2022</h1>
-      <cite className="block text-gray-500 my-6 md:my-10">
+      {/*<cite className="block text-gray-500 my-6 md:my-10">
         Motto konferencie.
-      </cite>
+      </cite>*/}
       <p className="font-bold text-xl">
         8. – 9. september 2022<br/>
         Prírodovedecká fakulta Univerzity Komenského v Bratislave
@@ -22,7 +22,7 @@ export default function GeoKarto( props ) {
       
       <p><img src="images/geokarto2022/heroF.jpg" /></p>
   
-      <p>Konferencia je organizovaná každé dva roky Kartografickou spoločnosťou SR v spolupráci s významnými akademickými inštitúciami so zameraním na aktuálne otázky a trendy teórie a praxe kartografie, geoinformatiky a geodézie.</p>
+      <p>Konferencia je organizovaná každé dva roky Kartografickou spoločnosťou SR v spolupráci s významnými akademickými inštitúciami so zameraním na aktuálne otázky a trendy teórie a praxe kartografie, geoinformatiky a geodézie. V roku 2022 sa  bude konferencia organizovať v spolupráci s Katedrou fyzickej geografie a geoinformatiky, Prírodovedeckej fakulty UK v Bratislave.</p>
       
       <p className="text-center-f">
         <Link href="">
@@ -42,17 +42,17 @@ export default function GeoKarto( props ) {
       </p>
       
       <p className="flex flex-col md:flex-row md:justify-around">
-        <Link href="https://forms.gle/tqdEWxVAgCNgNXwv7">
-          <button className="bg-k-blue px-5 py-2 rounded-full m-4 text-base text-white justify-self-end hover:shadow-md">
+        <a href="https://forms.gle/tqdEWxVAgCNgNXwv7" target="_blank" className="m-4 ">
+          <button className="bg-k-blue px-5 py-2 rounded-full text-base text-white justify-self-end hover:shadow-md">
             Registrovať sa ako prednášajúci
           </button>
-        </Link>
+        </a>
         {" "}
-        <Link href="">
-          <button className="bg-k-blue px-5 py-2 rounded-full m-4 text-base text-white justify-self-end hover:shadow-md">
+        <a href="https://forms.gle/empwf2nU9zVYzYaNA" target="_blank" className="m-4 ">
+          <button className="bg-k-blue px-5 py-2 rounded-full text-base text-white justify-self-end hover:shadow-md">
             Registrovať sa ako poslucháč
           </button>
-        </Link>
+        </a>
       </p>
       <p> </p>
       
@@ -66,7 +66,7 @@ export default function GeoKarto( props ) {
         <li>Štandardizácia, infraštruktúry, metadáta, geodatabázy</li>
         <li>Open-source GIS a otvorený digitálny obsah</li>
         <li>Webová kartografia, geovizualizácia a publikovanie geodát</li>
-        <li>Diaľkový prieskum Zeme a spracovanie obrazu</li>
+        <li>Diaľkový prieskum Zeme a spracovanie obrazu k tvorbe tematických máp</li>
         <li>Informačné systémy o území a kataster</li>
         <li>Geopriestorové analýzy a modelovanie</li>
         <li>Historické a staré mapy</li>
@@ -102,7 +102,7 @@ export default function GeoKarto( props ) {
         
         <h3 className="border-l-3 md:border-l-4 border-k-blue pl-3 md:pl-5 pt-2 md:pt-3 pb-1">Organizačné pokyny</h3>
         
-        <p>Konferencia sa koná...</p>
+        <p>Konferencia sa bude konať prezenčnou formou.</p>
         
         <h4> Účastnícky poplatok</h4>
         
@@ -131,7 +131,13 @@ export default function GeoKarto( props ) {
         </ul>
         
         <p>Účastníci konferencie si <b>ubytovanie</b> zabezpečujú individuálne. </p>
-        <p>Možnosti ubytovania ...</p>
+        <p>Možnosti ubytovania (orientačné ceny za osobu a noc):</p>
+        <ul>
+          <li>Hotel Družba: 33 EUR</li>
+          <li>Botel Pressburg: 52 EU</li>
+          <li>Hotel SOREA REGIA: 71 EUR</li>
+          <li>Park Inn by Radisson Danube Bratislava: 77 EUR</li>
+        </ul>
         
         <h4>Platby</h4>
         <ul>
@@ -162,16 +168,17 @@ export default function GeoKarto( props ) {
         <h3 className="border-l-3 md:border-l-4 border-k-blue pl-3 md:pl-5 pt-2 md:pt-3 pb-1">Organizátori a odborní garanti</h3>
         
         <div className="flex flex-row items-stretch justify-around space-x-3 md:space-x-4 py-4">
-          <Link href="/" >
-            <a className="flex flex-col justify-center">
-              <img className="w-44 md:w-52" src="images/bezLogoO.svg" alt="Katedra fyzickej geografie a geoinformatiky" />
-            </a>
-          </Link>
-          <a href="https://www.slovakcarto.sk/" className="flex flex-col items-center justify-end">
+          <a href="https://www.slovakcarto.sk/" target="_blank" className="flex flex-col items-center justify-end">
             <img className="h-16 md:h-28" src="images/geokarto2022/KSSRlogo.svg" alt="Kartografická spoločnosť Slovenskej republiky" />
             <span className="text-xs md:text-base text-center">Kartografická spoločnosť SR</span>
           </a>
-          <a href="http://www.geography.sav.sk/" className="flex flex-col items-center justify-end">
+        
+          <a href="https://fns.uniba.sk/" target="_blank" className="flex flex-col items-center justify-end">
+            <img className="h-16 md:h-28" src="images/geokarto2022/prifUK.svg" alt="Prírodovedecká fakulta UK" />
+            <span className="text-xs md:text-base text-center">Prírodovedecká fakulta UK</span>
+          </a>
+          
+          <a href="http://www.geography.sav.sk/" target="_blank" className="flex flex-col items-center justify-end">
             <img className="h-15 md:h-24" src="images/geokarto2022/GgUlogo.svg" alt="Geografický ústav SAV, v.v.i." />
             <span className="text-xs md:text-base text-center" >Geografický ústav SAV, v.v.i.</span>
           </a>
@@ -180,7 +187,7 @@ export default function GeoKarto( props ) {
         <h4>Organizačný výbor</h4>
         <p>
           Mgr. Miroslav Kožuch, PhD. (PriF UK Bratislava)<br/>
-          Mgr. Richard Feciskanin, PhD. (PriF UK Bratislava)<br/>
+          Mgr. Richard Feciskanin, Ph.D. (PriF UK Bratislava)<br/>
           Mgr. Alexandra Benová, PhD. (PriF UK Bratislava)<br/>
           Ing. Róbert Fencík, PhD. (SvF STU Bratislava)<br/>
           Ing. Daniel Szatmári, PhD. (GgÚ SAV Bratislava)
@@ -192,6 +199,7 @@ export default function GeoKarto( props ) {
           doc. RNDr. Ján Feranec, DrSc. (GgÚ SAV Bratislava)<br/>
           prof. Mgr. Jaroslav Hofierka, PhD. (PF UPJŠ Košice) <br/>
           doc. RNDr. Dagmar Kusendová, CSc. (PriF UK Bratislava) <br/>
+          doc. RNDr. Igor Matečný, PhD. (PriF UK Bratislava) <br/>
           Mgr. Ľuboslav Michalík (GKÚ Bratislava) <br/>
           prof. Ing.  Ján Tuček, PhD. (LF TU Zvolen) <br/>
           prof. RNDr. Vít Voženílek, CSc. (UPOL Olomouc)
