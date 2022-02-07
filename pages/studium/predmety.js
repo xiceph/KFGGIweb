25dfca597566e2ff015e2e4498713041cccef9a5
Richard Feciskanin <richardfeciskanin@gmail.com>
2022-02-07 12:01:27 +0100

    update predmety

--- pages/studium/predmety.js
+++ pages/studium/predmety.js
@@ -1,37 +1,18 @@
-import { promises as fs } from 'fs';
 import path from 'path';
 import Link from "next/link";
 
-function Predmety({ data }) {
+function Predmety() {
   
   return (
     <div className="w-full">
-      <h1>Zoznam predmetov bakalárskeho štúdia</h1>
-      <dl>
-        {data.map( item => (
-          <>
-            <dt key={item.addr}>
-              <Link href={`/studium/predmety/${item.addr}`}>
-                {item.name}
-              </Link>
-            </dt>
-            <dd key={`${item.addr}_d`} className="mb-3">{`vyučujúci: ${item.teacher}, typ: povinne voliteľný, kredity: 5, semester: Z`}</dd>
-          </>
-        ))}
-      </dl>
+      <h1>Predmety bakalárskeho štúdia</h1>
+      <div className="w-full overflow-y-auto">
+        <img className="predm" src="images/predmety.svg"/>
+      </div>
     </div>
   )
 }
 
-export async function getStaticProps(context) {
 
-  const dataDirectory = path.join(process.cwd(), 'data');
-  const fileContents = await fs.readFile(`${dataDirectory}/predmety.json`, 'utf8');
-  const data = JSON.parse(fileContents);
-
-  return {
-    props: { data }, 
-  }
-}
 
 export default Predmety
