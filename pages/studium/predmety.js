8d436f87cfe4b446d0f4c23b98807dcba81c3189
Richard <richard.feciskanin@uniba.sk>
2022-02-08 16:10:49 +0100

    update predmety

--- pages/studium/predmety.js
+++ pages/studium/predmety.js
@@ -1,14 +1,18 @@
-import { useState } from 'react'; 
+import { useState, useEffect } from 'react'; 
 import Link from "next/link";
 import { SchemaPredmety } from "@components/schemaPredmety";
 
 function Predmety() {
-  const [sums, setSums] = useState({})
+  const [sums, setSums] = useState({all: [0,0,0,0,0,0], b: 0})
+  
+  useEffect(() => {
+    calcSums();
+  }) 
   
   const calcSums = () => {
     const select = document.querySelectorAll('.predmet.on');
     let sum = [0,0,0,0,0,0];
-    let sumB = [0,0,0,0,0,0];
+    let sumB = 0;
     select.forEach(item => {
       const year = item.id.substring(1);
       console.log(year)
@@ -16,19 +20,30 @@ function Predmety() {
       const pov = item.querySelector('.povinnost').textContent;
       sum[year-1] += kredit;
       if(pov == 'B'){
-        sumB[year-1] += kredit;
+        sumB += kredit;
       }
     })
     setSums({all: sum, b: sumB});
   }
+  const all = sums.all.reduce((a, b) => a + b, 0);
   
   return (
     <div className="w-full">
       <h1>Predmety bakalárskeho štúdia</h1>
       <div>
-      {JSON.stringify(sums)}
+        Kredity spolu (min 180): <b>{all}</b>{`   `}
+        za B predmety (min 19): <b>{sums.b}</b>
       </div>
       <div className="w-full overflow-y-auto">
+        <div className="predm flex flex-row">
+        {
+          sums.all.map((item, i) => (
+            <div className="flex-1 px-3 pt-2 pb-1">{i+1}. semester{` `} 
+              <b>{sums.all[i]}</b>
+            </div>
+          ))
+        }
+        </div>
         <SchemaPredmety className="predm" onClick={calcSums} />
       </div>
     </div>
