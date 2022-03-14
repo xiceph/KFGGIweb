32f6a5a8629151b2b69386c05e3b6deed4370e0e
Richard Feciskanin <richardfeciskanin@gmail.com>
2022-03-14 09:16:07 +0100

    update

---
+++ pages/studium/predmety_list.js
@@ -0,0 +1,16 @@
+import path from 'path';
+import Link from "next/link";
+
+function Predmety() {
+  
+  return (
+    <div className="w-full">
+      <h1>Predmety bakalárskeho štúdia</h1>
+      <div>i</div>
+    </div>
+  )
+}
+
+
+
+export default Predmety
