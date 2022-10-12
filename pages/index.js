72286643732f5c5853fe57c84142faff3a584a00
Richard Feciskanin <richardfeciskanin@gmail.com>
2022-10-12 08:41:43 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ function IndexPage( props ) {
                <div>
                  <div className="mb-2">Čas: 15:00 – 17:30 <br/><i>online cez MS Teams</i>, <i>prezenčne na PriF UK, B1-PLUS</i> </div>
                  <div className="mb-2">Prednášajúci: <i>PaedDr. Ján Štubňa, PhD.</i></div>
-                 <div className="mb-2">Prihláste sa na: <a href="https://lnk.sk/rar6" target="_blank">https://lnk.sk/rar6</a></div>
+                 <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/rar6" target="_blank">https://lnk.sk/rar6</a></div>
                  <div className="mb-8"><a href="https://fns.uniba.sk/typo3temp/pics/2022-10-26-geovedy_adc028f25e.jpg" target="_blank">Plagát</a></div>
                </div>
                
