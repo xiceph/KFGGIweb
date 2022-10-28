49a3a07c45c62bbd2d80eb9c1f920d704a723a0d
Richard Feciskanin <richardfeciskanin@gmail.com>
2022-10-28 14:49:04 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -93,8 +93,8 @@ function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              {/*<p className="text-gray-600 italic">žiadne udalosti</p>*/}
-              <div className="mb-1.5"><Datum date={ new Date("2022-10-26") } today={ new Date(props.today) }></Datum></div>
+              <p className="text-gray-600 italic">žiadne udalosti</p>
+              {/*<div className="mb-1.5"><Datum date={ new Date("2022-10-26") } today={ new Date(props.today) }></Datum></div>
                <h5 className="font-bold mb-2">Prednáška</h5>
                <p className="font-bold mb-f2">Drahé kamene – známe i neznáme</p>
                <div>
@@ -102,7 +102,7 @@ function IndexPage( props ) {
                  <div className="mb-2">Prednášajúci: <i>PaedDr. Ján Štubňa, PhD.</i></div>
                  <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/rar6" target="_blank">https://lnk.sk/rar6</a></div>
                  <div className="mb-8"><a href="https://fns.uniba.sk/typo3temp/pics/2022-10-26-geovedy_bd4fd833fe.jpg" target="_blank">Plagát</a></div>
-               </div>
+               </div>*/}
                
              {/* <div className="mb-1.5"><Datum date={ new Date("2022-08-24") } _today={ new Date(props.today) }></Datum></div>
               <h5 className="font-bold mb-1">Štátne skúšky</h5>
