881ed49318ea4b21d0a0c56a75fc47764b5c970f
Richard Feciskanin <richardfeciskanin@gmail.com>
2023-01-19 09:00:29 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -96,8 +96,8 @@ function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              {/*<p className="text-gray-600 italic">žiadne udalosti</p> */}
-              <div className="mb-1.5"><Datum date={ new Date("2023-01-18") } today={ new Date(props.today) }></Datum></div>
+              <p className="text-gray-600 italic">žiadne udalosti</p> 
+              {/*<div className="mb-1.5"><Datum date={ new Date("2023-01-18") } today={ new Date(props.today) }></Datum></div>
                <h5 className="font-bold mb-2">Prednáška</h5>
                <p className="font-bold mb-f2">Aktuálna ekovýchova v DAPHNE – inšpirácie, skúsenosti a výzvy</p>
                <div>
@@ -105,7 +105,7 @@ function IndexPage( props ) {
                  <div className="mb-2">Prednášajúca: <i>Mgr. Jana Menkynová</i></div>
                  <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/msys" target="_blank">https://lnk.sk/msys</a></div>
                  <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2023/2023-01-18-geovedy.jpg" target="_blank">Plagát</a></div>
-               </div>
+               </div>*/}
               
              {/* <div className="mb-1.5"><Datum date={ new Date("2022-08-24") } _today={ new Date(props.today) }></Datum></div>
               <h5 className="font-bold mb-1">Štátne skúšky</h5>
