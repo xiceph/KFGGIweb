bf10fb0e597065201ae4fe6f33e93e4531cf94f1
Richard Runner <raner@gis.fns.uniba.sk>
2023-02-16 08:55:44 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -96,8 +96,8 @@ function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              {/*<p className="text-gray-600 italic">žiadne udalosti</p> */}
-              <div className="mb-1.5"><Datum date={ new Date("2023-02-15") } today={ new Date(props.today) }></Datum></div>
+              <p className="text-gray-600 italic">žiadne udalosti</p>
+              {/*<div className="mb-1.5"><Datum date={ new Date("2023-02-15") } today={ new Date(props.today) }></Datum></div>
                <h5 className="font-bold mb-2">Prednášky</h5>
                <p className="font-bold mb-f1">Ďaleké výhľady  – kedy „začínajú“ a kde „končia“</p>
                <p className="font-bold mb-f2 mt-f0">Putovanie Nepálom</p>
@@ -106,7 +106,7 @@ function IndexPage( props ) {
                  <div className="mb-2">Prednášajúci: <i>RNDr. Dalibor Výberči</i>, <i>RNDr. Ján Košiar</i></div>
                  <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/emm0" target="_blank">https://lnk.sk/emm0</a></div>
                  <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2023/2023-02-15-geovedy.jpg" target="_blank">Plagát</a></div>
-               </div>
+               </div>*/}
               
              {/* <div className="mb-1.5"><Datum date={ new Date("2022-08-24") } _today={ new Date(props.today) }></Datum></div>
               <h5 className="font-bold mb-1">Štátne skúšky</h5>
