e1fc3f54fee35d5f901063fb6ebf8a150e7c7ce8
Richard Feciskanin <richard.feciskanin@uniba.sk>
2023-04-11 10:22:32 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -103,15 +103,15 @@ function IndexPage( props ) {
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
               {/*<p className="text-gray-600 italic">žiadne udalosti</p>*/}
-              <div className="mb-1.5"><Datum date={ new Date("2023-03-29") } today={ new Date(props.today) }></Datum></div>
+              <div className="mb-1.5"><Datum date={ new Date("2023-04-19") } today={ new Date(props.today) }></Datum></div>
                <h5 className="font-bold mb-2">Prednáška</h5>
-               <p className="font-bold mb-f1">Fenomény krasu Malých Karpát</p>
+               <p className="font-bold mb-f1">Plesá ako svedkovia zmien tatranskej prírody</p>
                {/*<p className="font-bold mb-f2 mt-f0">Putovanie Nepálom</p>*/}
                <div>
                  <div className="mb-2">Čas: 15:00 – 17:30 <br/><i>online cez MS Teams</i>, <i>prezenčne na PriF UK, B1-AMOS</i> </div>
-                 <div className="mb-2">Prednášajúci: <i>RNDr. Alexander Lačný, PhD.</i></div>
-                 <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/ogms" target="_blank">https://lnk.sk/ogms</a></div>
-                 <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2023/2023-03-29-geovedy.jpg" target="_blank">Plagát</a></div>
+                 <div className="mb-2">Prednášajúci: <i>Dr. Radovan Kyška-Pipík, PhD.</i></div>
+                 <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/alde" target="_blank">https://lnk.sk/alde</a></div>
+                 <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2023/2023-04-19-geovedy.jpg" target="_blank">Plagát</a></div>
                </div>
               
              <div className="mb-1.5"><Datum date={ new Date("2023-06-05") } _today={ new Date(props.today) }></Datum></div>
