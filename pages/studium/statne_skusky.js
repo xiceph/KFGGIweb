eceb8d61839f3f4fde5a8a69719b2148598945e9
Richard Feciskanin <richard.feciskanin@uniba.sk>
2023-06-07 08:24:03 +0200

    update

--- pages/studium/statne_skusky.js
+++ pages/studium/statne_skusky.js
@@ -10,9 +10,9 @@ export default function Statnice() {
         <h3 className="border-l-3 md:border-l-4 border-k-brown pl-3 md:pl-5 py-2 md:py-3">Bakalárske študijné programy</h3>
         
         <h4>Termín 2022/23</h4>
-        <p>Obhajoba bakalárskej práce ZKI/FYZ: 15. 6. 2023 8:00 – 14:30 </p>
+        <p>Obhajoba bakalárskej práce ZKI/FYZ: 15. 6. 2023 8:00 – 14:30 (<a href="docs/HarmonogramFYZBc2023.pdf" target="_blank">harmonogram [pdf]</a>)</p>
         <p>Obhajoba bakalárskej práce ZKI + upGe/GIS: 14. 6. 2023 od 8:30</p>
-        {/*<p>Obhajoba bakalárskej práce ZEP:  15. 6. 2022 (<a href="docs/HarmonogramFYZBc2022.pdf" target="_blank">harmonogram [pdf]</a>), 24. 8. 2022.</p>
+        {/*<p>Obhajoba bakalárskej práce ZEP:  15. 6. 2022 , 24. 8. 2022.</p>
         <p>Obhajoba bakalárskej práce ZKI:  16. 6. 2022, 24. 8. 2022.*/}{/*22. 6. 2021 (<a href="docs/ObhajobyBc2021.pdf" target="_blank">harmonogram [pdf]</a>), 24. 8. 2021 (<a href="docs/Obhajoby2021aug.pdf" target="_blank">harmonogram [pdf]</a>).*/}
         
         <h4>Povinné predmety</h4>
