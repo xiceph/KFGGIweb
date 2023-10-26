8f6d1c6d0e9406e20c5b4a4a3de2347f20073645
Richard Feciskanin <richard.feciskanin@uniba.sk>
2023-10-26 13:42:10 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -102,11 +102,11 @@ function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              {/*<p className="text-gray-600 italic">žiadne udalosti</p>*/}
-              <div className="mb-1.5"><Datum date={ new Date("2023-10-25") } today={ new Date(props.today) }></Datum></div>
+              <p className="text-gray-600 italic">žiadne udalosti</p>
+              {/*<div className="mb-1.5"><Datum date={ new Date("2023-10-25") } today={ new Date(props.today) }></Datum></div>
                 <h5 className="font-bold mb-2">Prednáška</h5>
                 <p className="font-bold mb-f1">Železničné prevádzky – špecifický typ environmentálnych záťaží</p>
-                {/*<p className="font-bold mb-f2 mt-f0">Pripravované zmeny kurikula a podpora výučby geológie na ZŠ a SŠ</p>*/}
+                <p className="font-bold mb-f2 mt-f0">Pripravované zmeny kurikula a podpora výučby geológie na ZŠ a SŠ</p>
               <div>
                 <div className="mb-2">Čas: 15:00 – 17:00 <br/><i>online cez MS Teams</i>, <i>prezenčne na PriF UK, B1-PLUS</i> </div>
                 <div className="mb-2">Prednášajúci: <i>Mgr. Juraj Macek, PhD. </i></div>
@@ -114,7 +114,7 @@ function IndexPage( props ) {
                 <div className="mb-8"><a href="https://fns.uniba.sk/uploads/pics/2023-10-25-geovedy.jpg" target="_blank">Plagát</a></div>
               </div>
 
-             {/*<div className="mb-1.5"><Datum date={ new Date("2023-08-30") } _today={ new Date(props.today) }></Datum></div>
+             <div className="mb-1.5"><Datum date={ new Date("2023-08-30") } _today={ new Date(props.today) }></Datum></div>
               <h5 className="font-bold mb-1">Obhajoba dizertačnej práce</h5>
               <div className="mb-8">Od 10:00 sa uskutoční obhajoba Mgr. Veroniky Beranovej. <Link href="/docs/Oznam_Beranova.pdf"><a target="_blank">Detailný oznam</a></Link>.</div>
 
