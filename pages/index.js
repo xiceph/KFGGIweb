16404441e861482b86f63b675ffeb5148204c803
Richard Feciskanin <richard.feciskanin@uniba.sk>
2023-12-14 08:41:40 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -111,17 +111,17 @@ function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              {/*<p className="text-gray-600 italic">žiadne udalosti</p>*/}
-              <div className="mb-1.5"><Datum date={ new Date("2023-12-13") } today={ new Date(props.today) }></Datum></div>
+              <p className="text-gray-600 italic">žiadne udalosti</p>
+              {/*<div className="mb-1.5"><Datum date={ new Date("2023-12-13") } today={ new Date(props.today) }></Datum></div>
                 <h5 className="font-bold mb-2">Prednáška</h5>
                 <p className="font-bold mb-f1">Ochrana prírody a krajiny v európskom kontexte</p>
-                {/*<p className="font-bold mb-f2 mt-f0">Pripravované zmeny kurikula a podpora výučby geológie na ZŠ a SŠ</p>*/}
+                {/*<p className="font-bold mb-f2 mt-f0">Pripravované zmeny kurikula a podpora výučby geológie na ZŠ a SŠ</p>//}
               <div>
                 <div className="mb-2">Čas: 15:00 – 17:00 <br/><i>online cez MS Teams</i>, <i>prezenčne na PriF UK, B1-PLUS</i> </div>
                 <div className="mb-2">Prednášajúci: <i>Mgr. Marta Nevřelová, PhD. </i></div>
                 <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/fkqm" target="_blank">https://lnk.sk/fkqm</a></div>
                 <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2023/2023-12-13-geovedy.jpg" target="_blank">Plagát</a></div>
-              </div>
+              </div>*/}
 
               {/*}<div className="mb-1.5"><Datum date={ new Date("2023-08-30") } _today={ new Date(props.today) }></Datum></div>
               <h5 className="font-bold mb-1">Obhajoba dizertačnej práce</h5>
