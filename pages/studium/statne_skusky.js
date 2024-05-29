955f0f5c1dd1617bb7a9a43abacf11df0267de88
Richard Feciskanin <richard.feciskanin@uniba.sk>
2024-05-29 10:16:38 +0200

    update

--- pages/studium/statne_skusky.js
+++ pages/studium/statne_skusky.js
@@ -9,10 +9,10 @@ export default function Statnice() {
       <Contents levels={1} >
         <h3 className="border-l-3 md:border-l-4 border-k-brown pl-3 md:pl-5 py-2 md:py-3">Bakalárske študijné programy</h3>
         
-        <h4>Termín 2022/23</h4>
-        <p>Obhajoba bakalárskej práce ZKI/FYZ: 15. 6. 2023 8:00 – 14:30 (<a href="docs/HarmonogramFYZBc2023.pdf" target="_blank">harmonogram [pdf]</a>), 23. 8. 2023 9:00 (<a href="docs/HarmonogramFYZGIS2023A.pdf" target="_blank">harmonogram [pdf]</a>)</p>
-        <p>Obhajoba bakalárskej práce ZKI + upGe/GIS: 14. 6. 2023 od 8:30 (<a href="docs/HarmonogramGISBc2023.pdf" target="_blank">harmonogram [pdf]</a>)</p>
-        {/*<p>Obhajoba bakalárskej práce ZEP:  15. 6. 2022 , 24. 8. 2022.</p>
+        <h4>Termín 2023/24</h4>
+        <p>Obhajoba bakalárskej práce: 12. 6. 2024 8:00 – 15:30, 13. 6. 2024 8:00 – 11:20 (<a href="docs/ss_bc_harmonogram.jpg" target="_blank">harmonogram [jpg]</a>)</p>
+        {/*<p>Obhajoba bakalárskej práce ZKI + upGe/GIS: 14. 6. 2023 od 8:30 (<a href="docs/HarmonogramGISBc2023.pdf" target="_blank">harmonogram [pdf]</a>)</p>
+        <p>Obhajoba bakalárskej práce ZEP:  15. 6. 2022 , 24. 8. 2022.</p>
         <p>Obhajoba bakalárskej práce ZKI:  16. 6. 2022, 24. 8. 2022.*/}{/*22. 6. 2021 (<a href="docs/ObhajobyBc2021.pdf" target="_blank">harmonogram [pdf]</a>), 24. 8. 2021 (<a href="docs/Obhajoby2021aug.pdf" target="_blank">harmonogram [pdf]</a>).*/}
         
         <h4>Povinné predmety</h4>
