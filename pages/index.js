8c0f32d1202f97202cba331b334f1af4067da27a
Richard Feciskanin <richard.feciskanin@uniba.sk>
2025-02-03 09:38:31 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -121,18 +121,18 @@ function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              <p className="text-gray-600 italic">žiadne udalosti</p>
+              {/*<p className="text-gray-600 italic">žiadne udalosti</p>*/}
 
 
-              {/*<div className="mb-1.5"><Datum date={ new Date("2025-01-15") } today={ new Date(props.today) }></Datum></div>
+              <div className="mb-1.5"><Datum date={ new Date("2025-02-19") } today={ new Date(props.today) }></Datum></div>
               <h5 className="font-bold mb-2">Prednáška</h5>
-              <p className="font-bold mb-f1">Krasové územia Západných Karpát</p>
+              <p className="font-bold mb-f1">Platňová tektonika - história významnej myšlienky 20. storočia </p>
               <div>
                 <div className="mb-2">Čas: 15:00 – 17:00 <br/><i>online cez MS Teams</i>, <i>prezenčne na PriF UK, B1-PLUS</i> </div>
-                <div className="mb-2">Prednášajúci: <i>doc. RNDr. Alexander Lačný, PhD. </i></div>
-                <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/jxl8" target="_blank">https://lnk.sk/jxl8</a></div>
-                <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2025/2025-01-15-geovedy.jpg" target="_blank">Plagát</a></div>
-              </div>*/}
+                <div className="mb-2">Prednášajúci: <i>doc. RNDr. Rastislav Vojtko, PhD.</i></div>
+                <div className="mb-2">Pripojte sa na: <a href="https://lnk.sk/qsm9" target="_blank">https://lnk.sk/qsm9</a></div>
+                <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2025/2025-02-19-geovedy.jpg" target="_blank">Plagát</a></div>
+              </div>
 
               {/*<div className="mb-1.5"><Datum date={ new Date("2024-05-22") } today={ new Date(props.today) }></Datum></div>
                 <h5 className="font-bold mb-2">Prednáška</h5>
