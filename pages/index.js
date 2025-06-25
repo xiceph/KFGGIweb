9a5be19255d200900d20ee6c61406524540642d8
Richard Feciskanin <richard.feciskanin@uniba.sk>
2025-06-25 08:45:53 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -123,7 +123,7 @@ function IndexPage( props ) {
           <h3 className="text-gray-800">Najbližšie udalosti</h3>
           <div className="divide-y divide-k-xl-gray">
             <div className="py-4 first:pt-0">
-              {/*<p className="text-gray-600 italic">žiadne udalosti</p>*/}
+              <p className="text-gray-600 italic">žiadne udalosti</p>
 
 
               {/*<div className="mb-1.5"><Datum date={ new Date("2025-04-23") } today={ new Date(props.today) }></Datum></div>
@@ -147,9 +147,9 @@ function IndexPage( props ) {
                 <div className="mb-8"><a href="https://fns.uniba.sk/fileadmin/prif/kalendar/2025/2025-05-21-geovedy.jpg" target="_blank">Plagát</a></div>
               </div>*/}
 
-              <div className="mb-1.5"><Datum date={ new Date("2025-06-18") } _today={ new Date(props.today) }></Datum></div>
+              {/*<div className="mb-1.5"><Datum date={ new Date("2025-06-18") } _today={ new Date(props.today) }></Datum></div>
               <h5 className="font-bold mb-1">Štátne skúšky</h5>
-              <div>Na katedre budú 18. 6. 2025 a 19. 6. 2025 prebiehať <Link href="/studium/statne_skusky#sect_1"><a>štátne skúšky na bakalárskom študijnom programe</a></Link>.</div>
+              <div>Na katedre budú 18. 6. 2025 a 19. 6. 2025 prebiehať <Link href="/studium/statne_skusky#sect_1"><a>štátne skúšky na bakalárskom študijnom programe</a></Link>.</div>*/}
 
 {/*
               <div className="mb-1.5"><Datum date={ new Date("2024-02-15") } _today={ new Date(props.today) }></Datum></div>
