46c407cc178b429afdfe7a9dd75f4b638be02937
Richard Feciskanin <richard.feciskanin@uniba.sk>
2025-09-24 15:11:05 +0200

    update

--- pages/en/about/archives_library.js
+++ pages/en/about/archives_library.js
@@ -11,23 +11,21 @@ export default function ArchivesLibrary() {
         <p>Library of the Department consists of two parts:</p>
         <p><b>FYZ library</b> includes mostly literature related to physical geography and geoecology for the students and employees of the Department.</p> 
         <p><a href="https://liveuniba.sharepoint.com/:x:/s/Katedrafyzickejgeografieageoekolgie/EfC-TjTSDhpHkZQZNORKE0IBj19BI43et_DIHs3HjHggSQ?e=CmWaP2&isSPOFile=1" target="_blank">Catalogue <OpenExt /></a></p>
-        <p className="mb-f8">For your requests in the "FYZ" library, please contact Zora Machová, room B1-449, <Email parts={["zora.machova"]} /></p>
         
         <p><b>GIS library</b> is available for the students and employees of the Department, as well as all the employees of the Faculty of Natural Sciences. 
         The library includes both domestic and national publications related to cartography, geoinformatics, photogrammetry, remote sensing, mathematics, and physics.</p>
         <p>To become a member of the "GIS" library, you should fill out the form provided by a library administrator (for the registration, ID is required).</p>
         <p><a href="https://gis.fns.uniba.sk/?knkatalog" target="_blank">Catalogue <OpenExt /></a></p>
-        <p>For your requests in the "GIS" library, please contact Vladimír Pelech, room G-23, <Email parts={["pelech2"]} /></p>
         
         <h5>Requests</h5>
-        <p> For your literature requests, please contact the corresponding library administrator.</p>
+        <p> For your literature requests, please contact the library administrator: Hana Bobáľová, room G-19, <Email parts={["hana.bobalova"]} />.</p>
         
         
         <h3>Archive of theses</h3>
         <p>This library includes all the theses defended in our Department, on different degrees (bachelor's, master's, dissertation, rigorous, inaugural ...).</p>
         <p><a href="https://liveuniba.sharepoint.com/:x:/s/Katedrafyzickejgeografieageoekolgie/Ea1c62QzbatOhlD4JSRBwOMBp52XQd2k3X_c_9RcZQRKEw?e=lzb3St&isSPOFile=1" target="_blank">Catalogue <OpenExt /></a></p>
         <p>Theses can be studied only personally in the Department. For the interested person, the conference room (B1-444) will be accessed.</p>
-        <p>For your requests in the theses library, please contact Zora Machová, room B1-449, <Email parts={["zora.machova"]} /></p>
+        <p>For your requests in the theses library, please contact Hana Bobáľová, room G-19, <Email parts={["hana.bobalova"]} /></p>
         
         <h3>Archive of maps </h3>
         <h5>Topographic maps</h5>
